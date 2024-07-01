3af296f1cf303ce5b24dca2a1861ab6b159d14ec
Reinaldo Usadani <reinaldo.usadani@commsult.id>
2024-07-01 16:28:44 +0700

    Setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+let envFilePath: string = '.env';
+
+switch (`${process.env.NODE_ENV}`) {
+  case 'development': {
+    envFilePath = '.env.development.local';
+    break;
+  }
+  case 'test': {
+    envFilePath = '.env.test.local';
+    break;
+  }
+  default: {
+    Logger.log('NODE_ENV not set, using default env');
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ envFilePath })],
   controllers: [AppController],
   providers: [AppService],
 })
